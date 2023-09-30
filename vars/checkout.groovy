8f510d7a469408495cf7369084ad83db8d81bbc1
JEEVANBG <126395380+JEEVANBG@users.noreply.github.com>
2023-09-30 16:54:59 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,1 +1,11 @@
+def call (String git_branch = 'main' , String git_crendentials = null , String git_url = null) {
+    echo "This is one way , where we call this function within pipeline block"
+    checkout ([$Class: 'GitSCM',
+                branches: [[ name: "/*${git_branch}"]],
+                userRemoteConfigs:[[ credentialsID: "$git_crendentials", url: "$git_url"]]
+    ])
+    sh 'pwd; ls -lrt'
 
+}
+
+#This is one way of using shared library, without calling directly the library name.
