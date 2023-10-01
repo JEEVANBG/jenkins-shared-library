07f3e55865d721dbeb2ad55737f2e9b6f781da6f
JEEVANBG <126395380+JEEVANBG@users.noreply.github.com>
2023-10-01 10:26:51 +0530

    Update completeCheckout.groovy

--- vars/completeCheckout.groovy
+++ vars/completeCheckout.groovy
@@ -14,7 +14,7 @@ def call (String git_branch = 'main', String git_credentials = null , String git
                     echo "This is complete groovy checkout script , we don't need to define pipeline in jenkins machine"
                     checkout ([$class : 'GitSCM',
                                 branches : [[name: "*/${GIT_BRANCH}"]],
-                                userRemoteConfigs: [[ credentialsID: "$GIT_CREDENTIALS", url: "$GIT_URL"]]
+                                userRemoteConfigs: [[ credentialsID: "${GIT_CREDENTIALS}", url: "${GIT_UR}L"]]
                     ])
 
                     sh 'pwd; ls -lrt'
