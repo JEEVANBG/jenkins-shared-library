d6151f834eced2991b950720d3867d666ba60f90
JEEVANBG <126395380+JEEVANBG@users.noreply.github.com>
2023-10-01 14:01:14 +0530

    Update completeCheckout.groovy

--- vars/completeCheckout.groovy
+++ vars/completeCheckout.groovy
@@ -11,6 +11,7 @@ def call(String git_branch = 'main', String git_credentials = null, String git_u
         stages {
             stage('Checkout code') {
                 steps {
+                    sh 'pwd; ls -lrt'    
                      echo "This is dockerBuildComplete.groovy"
                      checkout([$class: 'GitSCM', 
                               branches: [[name: "*/${GIT_BRANCH}"]], 
