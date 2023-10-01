51d4d7acc7a183fcf7512e557b7bb58c092d23dd
JEEVANBG <126395380+JEEVANBG@users.noreply.github.com>
2023-10-01 13:35:15 +0530

    Update completeCheckout.groovy

--- vars/completeCheckout.groovy
+++ vars/completeCheckout.groovy
@@ -1,23 +1,22 @@
-def call (String git_branch = 'main', String git_credentials = null , String git_url = null) {
+def call(String git_branch = 'main', String git_credentials = null, String git_url = null) {    
     pipeline {
-        agent any
+        agent any 
 
         environment {
             GIT_BRANCH = "${git_branch}"
-            GIT_CREDENTIALS = "${git_credentials}"
+            GIT_CREDS = "${git_credentials}"
             GIT_URL = "${git_url}"
         }
 
         stages {
-            stage('Check_out_with_pipeline') {
-                steps {
-                    echo "This is complete groovy checkout script , we don't need to define pipeline in jenkins machine"
-                    checkout([$class: 'GitSCM',
-                               branches: [[name: "*/${GIT_BRANCH}"]],
-                               userRemoteConfigs: [[ credentialsId: "${GIT_CREDENTIALS}", url: "${GIT_URL}"]]
-                    ])
-
-                    sh 'pwd; ls -lrt'
+            stage('Checkout code') {
+                   echo "This is dockerBuildComplete.groovy"
+                    checkout([$class: 'GitSCM', 
+                 steps {
+                              branches: [[name: "*/${GIT_BRANCH}"]], 
+                              userRemoteConfigs: [[credentialsId: "${GIT_CREDS}", url: "${GIT_URL}"]]
+                            ])
+                    sh 'pwd; ls -lrt'        
                 }
             }
         }
