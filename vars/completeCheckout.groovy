032256aafb6be43420e47f1f3979deb611c17e5e
JEEVANBG <126395380+JEEVANBG@users.noreply.github.com>
2023-10-01 10:25:24 +0530

    Update completeCheckout.groovy

--- vars/completeCheckout.groovy
+++ vars/completeCheckout.groovy
@@ -10,9 +10,8 @@ def call (String git_branch = 'main', String git_credentials = null , String git
 
         stages {
             stage('Check_out_with_pipeline') {
-                
-                echo "This is complete groovy checkout script , we don't need to define pipeline in jenkins machine"
                 steps {
+                    echo "This is complete groovy checkout script , we don't need to define pipeline in jenkins machine"
                     checkout ([$class : 'GitSCM',
                                 branches : [[name: "*/${GIT_BRANCH}"]],
                                 userRemoteConfigs: [[ credentialsID: "$GIT_CREDENTIALS", url: "$GIT_URL"]]
