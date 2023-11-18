7e56cc255837bba81d70387bbe6eb458fa65b3cc
JEEVANBG <126395380+JEEVANBG@users.noreply.github.com>
2023-11-18 11:54:55 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,1 +1,8 @@
-
+def call (String git_branch = 'main', String git_credentials = 'gitlogin', String git_url = 'https://github.com/jaintpharsha/Devops-ITD-May-2023.git') {
+    echo "This is gitcheckout.groovy"
+    checkout([$class: 'GITSCM',
+            branches: [[name: "*/${git_branch}"]],
+            userRemoteConfigs: [[credentialsId: "${git_credentials}", url: "${git_branch}"]]
+    ])
+    sh "ls -lrt"
+}
